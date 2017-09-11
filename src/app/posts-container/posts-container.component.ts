081ee53ec7c1b0db692581b6da23d5def2132d1a
imran <imufun1@gmail.com>
2017-09-11 16:04:34 +0600

    component use

--- src/app/posts-container/posts-container.component.ts
+++ src/app/posts-container/posts-container.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit, ViewChild, Renderer} from '@angular/core';
 import {PostsService} from "../shared/posts.service";
 
 @Component({
-  selector: 'app-posts-container',
-  templateUrl: './posts-container.component.html',
-  styleUrls: ['./posts-container.component.css']
+    selector: 'app-posts-container',
+    templateUrl: './posts-container.component.html',
+    styleUrls: ['./posts-container.component.css']
 })
-export class PostsContainerComponent implements OnInit {
+export class PostsContainerComponent implements OnInit, AfterViewInit {
 
-  constructor(private postsService: PostsService) { }
+    @ViewChild('input1') input1;
 
-  ngOnInit() {
-      console.log(this.postsService.getName());
-  }
+    constructor(private renderer: Renderer) {
+    }
+
+    ngOnInit() {
+       // console.log(this.postsService.getName());
+    }
+
+    ngAfterViewInit(){
+        this.renderer.invokeElementMethod(
+            this.input1.nativeElement,
+            'focus',
+        );
+
+    }
 
 }
 
